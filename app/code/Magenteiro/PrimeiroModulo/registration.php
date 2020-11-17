e328b6d6180c3d06c4f714a742babfd034e47a2a
Flavio Santos <ti6@orthodonticbrasil.com.br>
2020-11-17 15:15:02 -0300

    Olá magenteiro!

---
+++ app/code/Magenteiro/PrimeiroModulo/registration.php
@@ -0,0 +1,6 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+$registrar = new ComponentRegistrar();
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Magenteiro_PrimeiroModulo', __DIR__);
