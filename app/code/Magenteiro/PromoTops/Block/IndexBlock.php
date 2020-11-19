3aa02c441c5cedf342f29f8802e7a2cc0f648957
Flavio Santos <ti6@orthodonticbrasil.com.br>
2020-11-19 13:09:25 -0300

    Magento Controller: front layout

---
+++ app/code/Magenteiro/PromoTops/Block/IndexBlock.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Magenteiro\PromoTops\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class IndexBlock extends Template
+{
+    public function agoraSim()
+    {
+        return "Agora sim, poxa vioda";
+    }
+}
--- app/code/Magenteiro/PromoTops/Controller/Tops/Index.php
+++ app/code/Magenteiro/PromoTops/Controller/Tops/Index.php
@@ -4,13 +4,21 @@
 namespace Magenteiro\PromoTops\Controller\Tops;
 
 
-use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
 
 class Index extends \Magento\Framework\App\Action\Action
 {
+    protected $pageFactory;
+
+    public function __construct(Context $context, PageFactory $pageFactory)
+    {
+        parent::__construct($context);
+        $this->pageFactory = $pageFactory;
+    }
 
     public function execute()
     {
-        echo "it works";
+        return $this->pageFactory->create();
     }
 }
